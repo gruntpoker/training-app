373555e829bfaa09aa31d0f8caf6b009277d3477
Michael Timmons <timmons02@gmail.com>
2019-11-04 00:41:25 -0500

    Clean up stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Button, Welcome } from '@storybook/react/demo';
-import RangeSelector from '../HandMatrix'
 import HandMatrix from '../HandMatrix';
 import PositionButtons from '../PositionButtons'
-import NavPositionSelector from '../NavPositionSelector'
-import { Router, Route } from 'react-router';
-import { MemoryRouter } from 'react-router';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('HandMatrix', module)
   .add('it renders', () => (
