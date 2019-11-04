0afad5218abaecc17ad5099d9f3b1138db39c76b
Michael Timmons <timmons02@gmail.com>
2019-11-04 15:16:21 -0500

    Rename range files

---
+++ src/open/index.js
@@ -0,0 +1,13 @@
+import pos1 from './pos_1.js'
+import pos2 from './pos_2.js'
+import pos3 from './pos_3.js'
+import pos4 from './pos_4.js'
+import pos5 from './pos_5.js'
+
+export default {
+    1: pos1,
+    2: pos2,
+    3: pos3,
+    4: pos4,
+    5: pos5
+}
