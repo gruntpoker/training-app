e822a5e97a5bedfb8f13a68f35ae6759df28f62a
Michael Timmons <timmons02@gmail.com>
2019-11-04 00:40:58 -0500

    Clean up stories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-
+import ScenarioList from './ScenarioList'
+import PositionButtons from './PositionButtons'
+import Analysis from './Analysis'
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,77 +16,18 @@ export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/analyze">
+            <Analysis></Analysis>
           </Route>
-          <Route path="/topics">
-            <Topics />
+          <Route path="/position">
+            <PositionButtons />
           </Route>
           <Route path="/">
-            <Home />
+            <ScenarioList />
           </Route>
         </Switch>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
 }
