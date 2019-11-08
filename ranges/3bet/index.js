add88e02463549d0debea268e2f0caeeb04562fd
Michael Timmons <timmons02@gmail.com>
2019-11-08 04:05:13 -0500

    Fix 3bet ranges

--- ranges/3bet/index.js
+++ ranges/3bet/index.js
@@ -7,9 +7,13 @@ const HJ_CO = require('./HJ_CO')
 const HJ_BTN = require('./HJ_BTN')
 const HJ_SB = require('./HJ_SB')
 const HJ_BB = require('./HJ_BB')
-const CO_BB = require('./CO_BTN')
+const CO_BTN = require('./CO_BTN')
 const CO_SB = require('./CO_SB')
-const CO_SB = require('./CO_BB')
+const CO_BB = require('./CO_BB')
+const BTN_SB = require('./BTN_SB')
+const BTN_BB = require('./BTN_SB')
+const SB_BB = require('SB_BB')
+
 module.exports = {
     UTG_BB,
     UTG_BTN,
@@ -20,6 +24,10 @@ module.exports = {
     HJ_BTN,
     HJ_SB,
     HJ_BB,
+    CO_BTN,
+    CO_SB,
     CO_BB,
-    CO_SB
+    BTN_BB,
+    BTN_SB,
+    SB_BB
 }
