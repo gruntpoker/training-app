8d9171db91813175068f7f28676e8f051b780c26
cmichaeltimmons <38392465+cmichaeltimmons@users.noreply.github.com>
2020-04-02 17:01:43 -0400

    fix build

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-const ranges = require('./ranges/index.js')
-const applyCashApi = require('./src/CashRanges/CashAPI')
-applyCashApi(app)
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+const ranges = require("./ranges/index.js");
+const applyCashApi = require("./src/CashRanges/CashAPI");
+applyCashApi(app);
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'))
-  })
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.listen(port)
-console.log('server started on port' + port)
+app.listen(port);
+console.log("server started on port" + port);
